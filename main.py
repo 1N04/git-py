443487c292dc9f8b87620e7b398fe2ad02296cfd
1N04 <ul2260201ju@gmail.com>
2025-09-04 16:41:08 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-x = int(input("1:"))
-y = int(input("2:"))
-c = input("+,-,*,/ :")
-
-if c == "+":
-    print(x + y)
-    if c == "-":
-        print(x - y)
-    if c == "*":
-        print(x * y)
-    if c == "/":
-        print(x / y)
+import random
+b = random.randint(0,100)
+c = 0
+while True:
+     a = int(input(f"{c}/{10} num:"))
+     if b < a:
+         print("меньше")
+     else:
+         c = c + 1
+     if b > a:
+         print("больше")
+     else:
+         c = c + 1
+     if b==a:
+         print("победил")
+     if c == 10:
+         print(f"{c}/{10} проиграл")
+         break

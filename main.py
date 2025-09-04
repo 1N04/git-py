b7d4a682857fcfce5b51f97faf5892a15919b4a7
1N04 <ul2260201ju@gmail.com>
2025-09-04 16:18:50 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+x = int(input("1:"))
+y = int(input("2:"))
+c = input("+,-,*,/ :")
+
+if c == "+":
+    print(x + y)
+    if c == "-":
+        print(x - y)
+    if c == "*":
+        print(x * y)
+    if c == "/":
+        print(x / y)
